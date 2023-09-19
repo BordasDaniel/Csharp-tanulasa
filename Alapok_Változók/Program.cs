7221260178e2a5c3b6de9dbc3e971174261fa32e
Bordás Dániel <79927720+BordasDaniel@users.noreply.github.com>
2023-09-19 22:49:31 +0200

    Add files via upload

---
+++ Alapok_Változók/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Alapok_Változók
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            // A változók különböző fajta adatok tárolására szolgálnak.
+            // Nagyon speciális tipúsu adatokat fogunk használni.
+            // Hogy engedélyezzenek bizonyos funkciókat ellátni azon a típuson
+            // Vagy ne pazaroljunk tárhelyet a memóriában.
+
+
+            // Változó létrehozására a szintaxis:
+            // típusa neve = értéke;
+
+            // bool (logikai) = true, false
+            bool szavazhatok = true;
+            // int (lehetnek 32 valamint 64 bitesek) = számok
+            // {index}: Helyörző itt a legvégén megadott paramétert fogja behelyezni a helyére sorrendben index szerint.
+            // pl.:
+            Console.WriteLine("Az én nevem {0}\nA korom {1}\nA város ahol születtem {2}", "József", 12, "Erdély");
+            
+            Console.WriteLine("A legnagyobb szám: {0}", int.MaxValue); // A legnagyobb tárolható szám
+            Console.WriteLine("A legkisebb szám: {0}", int.MinValue); // A legkisebb tárolható szám
+
+            // long = ezek is számok csak kifejezetten 64 bitet foglalnak le.
+
+            Console.WriteLine("A legnagyobb long: {0}", long.MaxValue);
+            Console.WriteLine("A legkisebb long: {0}", long.MinValue);
+
+            // decimal = tizedes pontosságú számok.
+            // M-el a végén kell lezárni.
+            // 128 bites értéket tárolnak = 28 számjegyű pontosságra képesek
+
+            decimal decPiÉrtéke = 3.14159265358979323846M;
+            decimal decNagySzam = 3.0000000000000000000000000011M;
+            Console.WriteLine("DEC : PI + NagySzam = {0}", decPiÉrtéke + decNagySzam);
+            Console.WriteLine("A legnagyobb decimal: {0}", decimal.MaxValue);
+            Console.WriteLine("A legkisebb decimal: {0}", decimal.MinValue);
+
+            // double = 64 bites float tipusú számok
+            // 32 bites = 14 számjegy pontosságú
+            Console.WriteLine("A legnagyobb double {0}", double.MaxValue);
+            Console.WriteLine("A legkisebb double {0}", double.MinValue);
+            double dblPiÉrtéke = 3.14159265358979;
+            double dblNagySzam = 3.00000000000000002;
+            Console.WriteLine("DBL : PI + NagySzam = {0}", dblPiÉrtéke + dblNagySzam);
+
+            // float = tizedes pontosságú számok.
+            // F-el a végén le kell zárni.
+            // 6 számjegy pontosságú
+            Console.WriteLine("A legnagyobb float {0}", float.MaxValue);
+            Console.WriteLine("A legkisebb float {0}", float.MinValue);
+            float floatPiÉrtéke = 3.14159265358979F;
+            float floatNagySzam = 3.00000000000000002F;
+            Console.WriteLine("Float : PI + NagySzam = {0}", floatPiÉrtéke + floatNagySzam);
+
+            // Más adattípusok:
+
+            // byte: 8-bit csak pozitiv szám(int) 0-tól 255-ig
+            Console.WriteLine("A legnagyobb byte: {0}\nA legkisebb byte: {1}", byte.MaxValue, byte.MinValue);
+
+            // char: 16-bit unicode karakter (egy karakter)
+
+            // sbyte: 8-bit pozitiv/negatív szám(int) 127-től -128-ig
+            Console.WriteLine("A legnagyobb sbyte: {0}\nA legkisebb sbyte: {1}", sbyte.MaxValue, sbyte.MinValue);
+
+            // short: 16-bit pozitiv/negativ szám(int) -32768-tól 32767-ig
+            Console.WriteLine("A legnagyobb short: {0}\nA legkisebb short: {1}", short.MaxValue, short.MinValue);
+
+            // uint: 32-bit csak pozitiv szám(int) 0-tól 4294967295-ig
+            Console.WriteLine("A legnagyobb uint: {0}\nA legkisebb uint: {1}", uint.MaxValue, uint.MinValue);
+
+            // ulong: 64-bit csak pozitiv szám(int) 0-tól 18446744073709551615-ig
+            Console.WriteLine("A legnagyobb ulong: {0}\nA legkisebb ulong: {1}", ulong.MaxValue, ulong.MinValue);
+
+            // ushort: 16-bit csak pozitiv szám(int) 0-tól 65535-ig
+            Console.WriteLine("A legnagyobb ushort: {0}\nA legkisebb ushort: {1}", ushort.MaxValue, ushort.MinValue);
+
+            // Típus módosítás:
+
+            // típusAmireSzeretnénk.Parse();
+            // érték.ToTípusAmireSzeretnénk();
+            // (típusAmireSzeretnénk)érték pl.: (int)1.32
+
+            // változó.GetType() változó típusának lekérdezése.
+
+            // Szövegből bool: Logikai érték lesz (csak true és false lehet a paraméter).
+            bool StrbőlBool = bool.Parse("true");
+            Console.WriteLine("StrbőlBool: {0}\nTípusa: {1}", StrbőlBool, StrbőlBool.GetType());
+
+            // Számból szöveg
+            int IntbőlString = int.Parse("100");
+            Console.WriteLine("IntbőlString: {0}\nTípusa: {1}", IntbőlString, IntbőlString.GetType());
+
+            // Dblből szöveg
+            double DblbőlString = double.Parse("1.234");
+            Console.WriteLine("DblbőlString: {0}\nTípusa: {1}", DblbőlString, DblbőlString.GetType());
+
+            // Dblből szöveg
+            string strÉrték = DblbőlString.ToString();
+            Console.WriteLine($"Adat típus: {strÉrték.GetType()}");
+
+            // Explicit konverzió:
+
+            // Azt nevezzük explicit konverziónak amikor néhány adatot elvesztünk
+
+            // Doublből int: Elvesztem az összes tizedesjegyet amit belül tárolt.
+            double Dblszam = 12.345;
+            Console.WriteLine($"Szám: {(int)Dblszam}"); // .345 elfog veszni.
+
+
+            // Implicit konverzió:
+
+            // Azt nevezzük implicit konverziónak amikor kisebb meretű tipusból nagyobbra váltunk.
+            // Ilyenkor nem veszítünk semmilyen adatot.
+
+            int IntSZAM = 10;
+            long LongSZAM = IntSZAM;
+            Console.WriteLine(LongSZAM);
+        } 
+    }
+}
---
+++ String_függvények/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace String_Függvények {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // string függvények:
+
+            string randomString = "Ez egy szöveg.";
+            Console.WriteLine("A szöveged hosszúsága: {0}", randomString.Length); // Hosszúságot adja vissza.
+            Console.WriteLine("A szöveged tartalmaz: {0}", randomString.Contains("egy")); // Boolean értékkel tér vissza, megnézi, hogy tartalmazza-e a szöveg az a szót vagy karaktert.
+            Console.WriteLine("Az indexe a : {0}", randomString.IndexOf("e")); // Számmal tér vissza, megnézi, hogy hányadik indexben helyezkedik el a szövegedben.
+            Console.WriteLine("Szöveg eltávolítása: {0}", randomString.Remove(0, 5)); // Eltávolítja a karaktereket a megadott intervallumon belül.
+            Console.WriteLine("Szöveg hozzáadása: {0}", randomString.Insert(10, "valami")); // Szöveg hozzáadása a megadott indexre.
+            Console.WriteLine("Helyettesítés: {0}", randomString.Replace("egy", "hülye")); // A megadott szöveg helyére kerül be.
+            Console.WriteLine("Hasonlítás A és B: {0}", string.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+            //  StringComparison.OrdinalIgnoreCase kikapcsolja a kis és nagy betűs szavak között különbséget.
+            // Hasonlítások és kis és nagybetűs különbségek ignorálása:
+            // < 0: str1 megelőzi str2-t
+            // = : 0
+            // > 0 : str2 megelőzi str1-et
+
+            Console.WriteLine("A == a: {0}", string.Equals("A", "a"));
+            Console.WriteLine("A == a: {0}", string.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Jobbra: {0}", randomString.PadLeft(20, '.')); // Karakterrel való kitöltés jobbra.
+            Console.WriteLine("Balra : {0}", randomString.PadRight(20, '.')); // Karakterrel való kitöltés balra. 
+            Console.WriteLine("Szóköz eltávolítása : {0}", randomString.Trim()); // Szóköz eltávolítása
+            Console.WriteLine("Nagybetű: {0}", randomString.ToUpper()); // Nagybetűs lesz minden karakter.
+            Console.WriteLine("Kisbetűs: {0}", randomString.ToLower()); // Kisbetűs lesz minden karakter.
+
+            string ujString = string.Format("{0} látta a {1} {2} a {3}ban", "Béci", "Józsit", "vásárolni", "Tesco"); // Szöveg formázása, kiegészítéssel.
+            Console.WriteLine(ujString);
+            Console.Write(ujString + "\n"); // Új sor beszúrása
+
+            string nev2 = "Derek";
+            string nev3 = "Derek";
+            if (nev2.Equals(nev3, StringComparison.Ordinal)) // Két szó összehasonlítása.
+            {
+                Console.WriteLine("A nevek megegyeznek.");
+            }
+            else
+            {
+                Console.WriteLine("A két név nem egyezik meg.");
+            }
+
+
+            // adatkivitel formázása:
+
+            Console.WriteLine("Pénzérték : {0:c}", 23.455);
+            Console.WriteLine("Kiegészítés 0-val : {0:d4}", 23);
+            Console.WriteLine("3 decimális-ra való kerekítés : {0:f3}", 23.4555555556);
+            Console.WriteLine("Tagolás : {0:n4}", 2300);
+        }
+    }
+    
+
+}
